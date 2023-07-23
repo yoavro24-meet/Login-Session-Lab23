e77610877a1ec566fa433590ea9cfb261bd43ea1
yoavro24-meet <139694379+yoavro24-meet@users.noreply.github.com>
2023-07-23 11:43:42 +0300

    t
    t

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,7 +4,7 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/',methods = ['GET','POST'] ) # What methods are needed?
 def home():
 	
 	return render_template('home.html')
@@ -19,13 +19,25 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html',quote = "q", autherquote = "athrq",autherage = "athra") # What variables are needed?
 
 
-@app.route('/thanks')
+@app.route('/thanks',methods = ['GET','POST'])
 def thanks():
+	try:
+	  login_session["q"] = quote
+	  login_session["athrq"] = autherquote
+	  login_session["athra"] = autherage
+	  return render_template('thanks.html')
 
-	return render_template('thanks.html')
+	
+	except:
+		return render_template('/error.html')
+
+
+
+	 
+	
 
 
 if __name__ == '__main__':
